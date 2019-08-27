fb1af1e147b914cf1a81d90ff8cd6c578c7b3a14
Ryan Walker <rytwalker@gmail.com>
2019-08-27 09:10:02 -0400

    routing async done

--- src/App.js
+++ src/App.js
@@ -1,12 +1,21 @@
 import React from "react";
 import { render } from "react-dom";
+import { Link, Router } from "@reach/router";
 import SearchParams from "./SearchParams";
+import Details from "./Details";
 
 export default function App() {
   return (
     <div>
-      <h1>Adopt Me</h1>
-      <SearchParams />
+      <header>
+        <Link to="/">
+          <h1>Adopt Me</h1>
+        </Link>
+      </header>
+      <Router>
+        <SearchParams path="/" />
+        <Details path="/details/:id" />
+      </Router>
     </div>
   );
 }
---
+++ src/Details.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Details = props => {
+  return (
+    <div>
+      <h1>Hi lol</h1>
+    </div>
+  );
+};
+
+export default Details;
